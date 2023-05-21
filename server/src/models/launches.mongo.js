b78a0b3742b5428c7a95865f616e1226c5999546
Tinsae Yohannes <60984788+tinsaeDotDev@users.noreply.github.com>
2023-05-21 19:03:05 +0300

    🛠️refactor(server) restructure launchesSchema in launches.mongo.js

--- server/src/models/launches.mongo.js
+++ server/src/models/launches.mongo.js
@@ -1,37 +1,40 @@
 const mongoose = require('mongoose');
 
-const launchesSchema = new mongoose.Schema({
-  flightNumber: {
-    type: Number,
-    required: true,
+const launchesSchema = new mongoose.Schema(
+  {
+    flightNumber: {
+      type: Number,
+      required: true,
+    },
+    launchDate: {
+      type: Date,
+      required: true,
+    },
+    mission: {
+      type: String,
+      required: true,
+    },
+    rocket: {
+      type: String,
+      required: true,
+    },
+    target: {
+      type: String,
+      required: true,
+    },
+    customers: [String],
+    upcoming: {
+      type: Boolean,
+      required: true,
+    },
+    success: {
+      type: Boolean,
+      required: true,
+      default: true,
+    },
   },
-  launchDate: {
-    type: Date,
-    required: true,
-  },
-  mission: {
-    type: String,
-    required: true,
-  },
-  rocket: {
-    type: String,
-    required: true,
-  },
-  target: {
-    type: String,
-    required: true,
-  },
-  customers: [String],
-  upcoming: {
-    type: Boolean,
-    required: true,
-  },
-  success: {
-    type: Boolean,
-    required: true,
-    default: true,
-  },
-});
+  { useFindAndModify: false }
+);
 
 //connects launchesSchema with the "launches" collection
-module.exports =  mongoose.model('Launch', launchesSchema);
+module.exports = mongoose.model('Launch', launchesSchema);
