da9b0275c6ce1d1010b0be49cdd191081e7e1cd5
Tinsae Yohannes <60984788+tinsaeDotDev@users.noreply.github.com>
2023-05-22 22:56:20 +0300

    ⚙️ chore(server): move mongoose connection to services/mongo\n This commit server.js to use the mongoConnect function from services/mongo instead of connecting to MongoDB directly using mongoose.

--- server/src/server.js
+++ server/src/server.js
@@ -1,30 +1,17 @@
 const http = require('http');
 
-const mongoose = require('mongoose');
-
 const app = require('./app');
 
 const { loadPlanetsData } = require('./models/planets.model');
+const { mongoConnect } = require('./services/mongo');
 
 const server = http.createServer(app);
 
 const PORT = process.env.PORT || 8000;
 
-const MONGO_URL =
-  'mongodb+srv://nasa_api:iHYmYxbNEiheqiPA@nasacluster.g8ohhol.mongodb.net/nasa?retryWrites=true&w=majority';
-
-mongoose.connection.once('open', () => {
-  console.log('MongoDB connection ready!');
-});
-
-mongoose.connection.on('error', (err) => {
-  console.error(err);
-});
-
 async function startServer() {
   try {
-    await mongoose.connect(MONGO_URL);
-
+    await mongoConnect();
     await loadPlanetsData();
 
     server.listen(PORT, () => {
