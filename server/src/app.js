a18e3ecb41d68de5302608dc53e28e5fccc26b33
Tinsae Yohannes <60984788+tinsaeDotDev@users.noreply.github.com>
2023-05-23 08:26:02 +0300

    🛠️refactor(server): restructure routes\n
    The changes made in this commit restructures the routes of the server, removing the planets and launches subroutes and replacing them with a single API route.

--- server/src/app.js
+++ server/src/app.js
@@ -3,8 +3,7 @@ const cors = require('cors');
 const path = require('path');
 const morgan = require('morgan');
 
-const planetsRouter = require('./routes/planets/planets.routes');
-const launchesRouter = require('./routes/launches/launches.routes');
+const api = require('./routes/api');
 
 const app = express();
 
@@ -19,8 +18,7 @@ app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
-app.use('/v1/planets', planetsRouter);
-app.use('/v1/launches', launchesRouter);
+app.use('/v1', api);
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
