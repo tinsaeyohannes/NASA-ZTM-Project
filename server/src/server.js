d74ce9b77837e374498af3cb8224efab68969c5a
Tinsae Yohannes <60984788+tinsaeDotDev@users.noreply.github.com>
2023-05-23 10:06:07 +0300

    🚀feat(server): add loadLaunchData call to startServer
    This commit adds a call to loadLaunchData function in the startServer method, allowing to load launch data on server start-up.

--- server/src/server.js
+++ server/src/server.js
@@ -4,7 +4,7 @@ const app = require('./app');
 
 const { loadPlanetsData } = require('./models/planets.model');
 const { mongoConnect } = require('./services/mongo');
-
+const { loadLaunchData } = require('./models/launches.model');
 const server = http.createServer(app);
 
 const PORT = process.env.PORT || 8000;
@@ -13,6 +13,7 @@ async function startServer() {
   try {
     await mongoConnect();
     await loadPlanetsData();
+    await loadLaunchData();
 
     server.listen(PORT, () => {
       console.log(`listening on port ${PORT}!`);
